a5f4faa0950b26780aaaf77300ee1e2bafbcb6b0
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-05-18 06:46:55 +0200

    add dbContext, dependancies and add the first migration to create database

--- PethanyApplication/Startup.cs
+++ PethanyApplication/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PethanyApplication.Models;
@@ -13,6 +15,12 @@ namespace PethanyApplication
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration Configuration)
+        {
+            configuration = Configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,6 +30,8 @@ namespace PethanyApplication
 
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
         }
 
