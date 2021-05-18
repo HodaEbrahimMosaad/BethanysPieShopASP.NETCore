dfa93c6acefb3f3a8fdfff20f18d1e2b7e497b05
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-05-18 07:08:27 +0200

    add notes prop to pie class and update db

--- PethanyApplication/Migrations/20210518050802_add-notes-prop-pie-class.Designer.cs
+++ PethanyApplication/Migrations/20210518050802_add-notes-prop-pie-class.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PethanyApplication.Models;
 
 namespace PethanyApplication.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210518050802_add-notes-prop-pie-class")]
+    partial class addnotesproppieclass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PethanyApplication/Models/Pie.cs
+++ PethanyApplication/Models/Pie.cs
@@ -20,5 +20,6 @@ namespace PethanyApplication.Models
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public string Notes { get; set; }
     }
 }
