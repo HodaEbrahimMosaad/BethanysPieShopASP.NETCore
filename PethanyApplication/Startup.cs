a331617e9f71af6c701d2eedb3cf1ef7425905aa
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-05-18 03:48:14 +0200

    set up mvc app

--- PethanyApplication/Startup.cs
+++ PethanyApplication/Startup.cs
@@ -25,15 +25,16 @@ namespace PethanyApplication
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
