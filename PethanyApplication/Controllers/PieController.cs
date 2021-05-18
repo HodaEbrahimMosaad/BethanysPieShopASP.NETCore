0d2f57a21f36bd3d7e8f15722a7c3c65203cad4e
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-05-18 05:27:36 +0200

    add pie controller and register repos in startup configure

---
+++ PethanyApplication/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using PethanyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PethanyApplication.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public ViewResult Index()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
