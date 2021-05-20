4158119777afcacdb507ea93f9b5ef31d519b257
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-05-20 06:59:47 +0200

    create PirCard partial view and home view

---
+++ PethanyApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PethanyApplication.Models;
+using PethanyApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PethanyApplication.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel()
+            {
+                PiesOfTheWeek = pieRepository.PiesOfTheWeek()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ PethanyApplication/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PethanyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PethanyApplication.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
